0957e6b4534f5b98532a998925e78f502e0385d5
Chris McFarlen <chris@mcfarlen.us>
2013-10-03 00:45:33 -0500

    testing bin

--- src/cncui/bin.clj
+++ src/cncui/bin.clj
@@ -2,6 +2,15 @@
   (:use [clojure.java.io])
   (:import [java.nio ByteBuffer]))
 
+(defn unsigned-byte [v]
+  (let [vv (unchecked-byte v)] (if (< vv 0) (+ 256 vv) vv)))
+
+(defn unsigned-short [v]
+  (let [vv (unchecked-short v)] (if (< vv 0) (+ 65536 vv) vv)))
+
+(defn unsigned-int [v]
+  (let [vv (unchecked-int v)] (if (< vv 0) (+ 4294967296 vv) vv)))
+
 (defn pack-byte [buf v]
   (.put buf (unchecked-byte (bit-and 0xff v)))
   buf)
@@ -12,7 +21,7 @@
 (defn pack-int [buf v]
   (.putInt buf (unchecked-int (bit-and 0xffffffff v))))
 
-(defn pack-str [buf s w]
+(defn pack-string [buf s w]
   (let [l (.length s)]
     (if (< l w)
       (-> buf
@@ -29,60 +38,35 @@
 (defn unpack-int [buf]
   (.getInt buf))
 
-(defn make-str-from-bytes [ba]
+(defn unpack-unsigned-byte [buf]
+  (unsigned-byte (unpack-byte buf)))
+
+(defn unpack-unsigned-short [buf]
+  (unsigned-short (unpack-short buf)))
+
+(defn unpack-unsigned-int [buf]
+  (unsigned-int (unpack-int buf)))
+
+(defn make-string-from-bytes [ba]
   (let [nba (into-array Byte/TYPE (map unchecked-byte (filter pos? (seq ba))))]
     (String. nba 0 (alength nba))))
 
-(defn unpack-str [buf w]
+(defn unpack-string [buf w]
   (let [bb (byte-array w)]
     (.get buf bb 0 w)
-    (make-str-from-bytes bb)))
-
-(def bb (->
-          (ByteBuffer/allocate 32)
-          (pack-byte 32)
-          (pack-byte 1)
-          (pack-byte 2)
-          (pack-byte 3)
-          (pack-byte -1)
-          (pack-byte -138)
-          (pack-byte 102)
-          (pack-byte 0xff)
-
-          (pack-short 32435)
-          (pack-short 1024)
-          (pack-short 32)
-          (pack-short 0)
-
-          (pack-int 0x12345678)
-          (pack-int 0x87654321)
-
-          (pack-str "hello" 4)
-          (pack-str "hi" 4)
-
-          (.flip)))
-
-(with-open [o (output-stream "test.bin")]
-  (.write o (.array bb)))
-
-(println 
-         [ (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-byte bb)
-           (unpack-short bb)
-           (unpack-short bb)
-           (unpack-short bb)
-           (unpack-short bb)
-           (unpack-int bb)
-           (unpack-int bb)
-           (unpack-str bb 4)
-           (unpack-str bb 4) ])
+    (make-string-from-bytes bb)))
 
 
+(defn pack-my-struct [values buf]
+  (reduce (fn [_ [f v]] (f buf v)) nil (map vector [pack-byte pack-byte pack-byte] values)))
+
+
+(defn struct-size
+  [s]
+  (reduce (fn [t c] (+ t
+                       (case c
+                         \b 1
+                         \h 2
+                         \i 4))) 0 (seq s)))
 
 
---
+++ test/cncui/bin_test.clj
@@ -0,0 +1,78 @@
+(ns cncui.bin-test
+  (:import [java.nio ByteBuffer])
+  (:use clojure.test
+        cncui.bin))
+
+(def ^:dynamic *b* 1)
+
+(defn myfixture [f]
+  (binding [*b* (ByteBuffer/allocate 32)]
+    (f)))
+
+(use-fixtures :each myfixture)
+
+(deftest packbytes
+  (testing "packing byte"
+    (let [d (range -127 127)]
+      (is (= d (map (fn [b] (-> *b*
+                            (.rewind)
+                            (pack-byte b)
+                            (.flip)
+                            (unpack-byte))) d))))))
+
+(deftest packubytes
+  (testing "packing unsigned byte"
+    (let [d (range 0 256)]
+      (is (= d (map (fn [b] (-> *b*
+                            (.rewind)
+                            (pack-byte b)
+                            (.flip)
+                            (unpack-unsigned-byte))) d))))))
+
+(deftest packshorts
+  (testing "packing shorts"
+    (let [d (range Short/MIN_VALUE Short/MAX_VALUE)]
+      (is (= d (map (fn [v] (-> *b*
+                            (.rewind)
+                            (pack-short v)
+                            (.flip)
+                            (unpack-short))) d))))))
+
+(deftest packushorts
+  (testing "packing unsigned shorts"
+    (let [d [0 10 Short/MAX_VALUE 65535]]
+      (is (= d (map (fn [v] (-> *b*
+                            (.rewind)
+                            (pack-short v)
+                            (.flip)
+                            (unpack-unsigned-short))) d))))))
+
+(deftest packints
+  (testing "packing ints"
+    (let [d [Integer/MIN_VALUE 0 Integer/MAX_VALUE]]
+      (is (= d (map (fn [v] (-> *b*
+                            (.rewind)
+                            (pack-int v)
+                            (.flip)
+                            (unpack-int))) d))))))
+
+(deftest packuints
+  (testing "packing unsigned ints"
+    (let [d [0 Integer/MAX_VALUE 4000000000 4294967295]]
+      (is (= d (map (fn [v] (-> *b*
+                            (.rewind)
+                            (pack-int v)
+                            (.flip)
+                            (unpack-unsigned-int))) d))))))
+
+
+(deftest packstrings
+  (testing "packing strings"
+    (let [s ["a" "b" "hello" "world"]]
+      (is (= s (map (fn [v] (-> *b*
+                            (.rewind)
+                            (pack-string v 32)
+                            (.flip)
+                            (unpack-string 32))) s))))))
+
+
